a86ededc29fd2fdeef2cecc92b750e478cdc5287
bymar <mgas@cesar.school>
2022-03-07 15:45:29 -0300

    pipe

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define QTD_MESSAGES 2
+
+int main() {
+
+    pid_t pid;
+    pid = fork();
+
+    int pipeFile[2];
+    int returnStatus;
+    char writeMessages[QTD_MESSAGES][20] = {"Maria nasceu", "Joao morreu"};
+    char readMessages[20];
+    memset(readMessages, '\0', sizeof(char) * 20);
+
+    returnStatus = pipe(pipeFile);
+
+    if (returnStatus == -1) {
+        printf("Error when create pipe\n");
+        return 1;
+    }
+    
+    if(pid > 0) {
+        for (int i = 0; i < QTD_MESSAGES; i++) {
+            printf("Writing Message %d is %s\n", i, writeMessages[i]);
+            write(pipeFile[1], writeMessages[0], sizeof(char) * 20);
+        }
+    } else {
+        for (int i = 0; i < QTD_MESSAGES; i++) {
+            read(pipeFile[0], readMessages, sizeof(char) * 20);
+            printf("Reading Message %d is %s\n", i, readMessages);
+        }
+    }
+
+    return 0;
+}
